55213e3ecf0e3b40540984e3cc1167d2de8bae88
Powrohitham Rushitha Datta <powrohithamrushithadatta@gmail.com>
2025-07-17 22:31:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 const connectDB=async() =>{
     try{
         await mongoose.connect(process.env.MONGODB_URL);
-        //console.log("Database connected");
+        console.log("Database connected");
     }catch(error){
-        //console.log(error)
+        console.log(error)
     }
 }
 export default connectDB;
